1b803a609ab7626727945aba763997391a9b162f
wanghaichao <hcwang1024@163.com>
2020-11-17 17:58:08 +0800

    拍照旋转

--- app/src/main/java/com/example/aliyun_identity_demo/MainActivity.java
+++ app/src/main/java/com/example/aliyun_identity_demo/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.aliyun_identity_demo;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+import androidx.core.os.EnvironmentCompat;
 
 import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.aliyun.identity.platform.api.IdentityCallback;
@@ -21,11 +32,17 @@ import com.aliyun.identity.platform.api.IdentityResponse;
 import com.aliyun.identity.platform.api.IdentityResponseCode;
 import com.aliyun.identity.platform.log.xLogger;
 import com.example.aliyun_identity_demo.net.NormalNetUtils;
+import com.example.aliyun_identity_demo.util.ImageUtil;
+import com.example.aliyun_identity_demo.util.PhotoBitmapUtil;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.logging.Logger;
 
@@ -33,6 +50,11 @@ import okhttp3.Call;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView ivPhoto;
+    private TextView textView;
+
+    private final static int  CAMERA_REQUEST_CODE = 1024;
 
     private static String LOG_TAG = "AliyunIdentity";
     private static final int IDENTITY_PERMISSION_REQUEST_CODE = 1024;
@@ -43,6 +65,9 @@ public class MainActivity extends AppCompatActivity {
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.READ_EXTERNAL_STORAGE,
     };
+
+    String mCameraImagePath = "";
+
 
     // 将metaInfo发送到业务自己的服务器端，服务器端调用阿里云相关接口拿到certifyId，再返回给客户端。
     // 注意：一个certifyId只能调用一次认证服务。
@@ -60,6 +85,11 @@ public class MainActivity extends AppCompatActivity {
 
         // 获取摄像头等权限
         handlePermissions();
+
+
+        ivPhoto = findViewById(R.id.iv_current);
+        textView = findViewById(R.id.tv_current);
+        setImageView();
 
         // 初始化SDK
         IdentityPlatform.getInstance().install(MainActivity.this);
@@ -95,6 +125,22 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+    }
+
+
+    private void setImageView( ){
+        try {
+            File photoFile = createImageFile();
+            if (photoFile != null) {
+                mCameraImagePath = photoFile.getAbsolutePath();
+                ivPhoto.setImageBitmap(BitmapFactory.decodeFile(mCameraImagePath));
+            }else {
+                textView.setText("请先进行拍照操作");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     /**
@@ -146,42 +192,187 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+
+
     public void OnNetClick(View view) {
 
+
+        if (TextUtils.isEmpty(mCameraImagePath)){
+            Toast.makeText(MainActivity.this,"请先进行拍照操作",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        ///common/aliyun/initSmartVerify（初始接口）
+        //  参数：String metaInfo
+        //
+        //
+        //  /common/aliyun/describeSmartVerify（获取是否成功接口）
+        //  参数：String certifyId
+
+        // 获取metaInfo，发送到业务自己服务器端换取certifyId
+        String metaInfo = IdentityPlatform.getMetaInfo(MainActivity.this);
+
+       String url = "http://10.10.130.151:8590/common/aliyun/initSmartVerify" ;
+
         Map<String,String> parmas =new HashMap<>();
+        parmas.put("metaInfo",metaInfo);
 //
-//        NormalNetUtils.getInstance().postDataAsynToNet("", parmas, new NormalNetUtils.MyNetCall() {
+//        NormalNetUtils.getInstance().getDataAsynFromNet("https://www.baidu.com", new NormalNetUtils.MyNetCall() {
 //            @Override
 //            public void success(Call call, Response response) throws IOException {
-//                Log.i("whcTag", "success: ");
+//                Log.i("whcTag", "success: " + response.body().string());
 //            }
 //
 //            @Override
 //            public void failed(Call call, IOException e) {
-//                Log.i("whcTag", "failed: ");
+//
 //            }
 //        });
-//
 
-        NormalNetUtils.getInstance().getDataAsynFromNet("https://www.baidu.com", new NormalNetUtils.MyNetCall() {
+
+
+//        NormalNetUtils.getInstance().getDataAsynFromNet(url, new NormalNetUtils.MyNetCall() {
+//            @Override
+//            public void success(Call call, Response response) throws IOException {
+//
+//            }
+//
+//            @Override
+//            public void failed(Call call, IOException e) {
+//
+//            }
+//        });
+
+
+        NormalNetUtils.getInstance().postDataAsynToNet(url, parmas, new NormalNetUtils.MyNetCall() {
             @Override
             public void success(Call call, Response response) throws IOException {
                 Log.i("whcTag", "success: " + response.body().string());
+
+                // 将metaInfo发送到业务自己服务器端换取certifyId
+                String certifyId = "";
+
+                // 开始验证
+                IdentityPlatform.getInstance().faceDetect(certifyId, null, new IdentityCallback() {
+                    @Override
+                    public boolean response(final IdentityResponse response) {
+                        if (IdentityResponseCode.IDENTITY_SUCCESS == response.code) {
+                            Toast.makeText(MainActivity.this,
+                                    "认证通过",
+                                    Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(MainActivity.this,
+                                    "认证失败([" + response.code + "]" + response.message + ")",
+                                    Toast.LENGTH_LONG).show();
+                        }
+
+                        return true;
+                    }
+                });
+
 
             }
 
             @Override
             public void failed(Call call, IOException e) {
-                Log.i("whcTag", "failed: " + e.toString());
-
+                Log.i("whcTag", "failed: ");
             }
         });
 
 
-
-
-
-
-
     }
+
+
+
+    /**
+     * 调起相机拍照
+     */
+    private void openCamera() {
+        Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // 判断是否有相机
+        if (captureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            Uri photoUri = null;
+
+                try {
+                    photoFile = createImageFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                if (photoFile != null) {
+                    mCameraImagePath = photoFile.getAbsolutePath();
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                        //适配Android 7.0文件权限，通过FileProvider创建一个content类型的Uri
+                        photoUri = FileProvider.getUriForFile(this, getPackageName() + ".fileprovider", photoFile);
+                    } else {
+                        photoUri = Uri.fromFile(photoFile);
+                    }
+                }
+
+                if (photoUri != null) {
+                captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                captureIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                startActivityForResult(captureIntent, CAMERA_REQUEST_CODE);
+            }
+        }
+    }
+
+    /**
+     * 创建保存图片的文件
+     * @return
+     * @throws IOException
+     */
+    private File createImageFile() throws IOException {
+    //    String imageName = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageName = "current";
+        ///storage/sdcard0/Pictures
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        if (!storageDir.exists()) {
+            storageDir.mkdir();
+        }
+        File tempFile = new File(storageDir, imageName);
+        if (!Environment.MEDIA_MOUNTED.equals(EnvironmentCompat.getStorageState(tempFile))) {
+            return null;
+        }
+
+        return tempFile;
+    }
+
+
+    public void OnTakePic(View view) {
+        openCamera();
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+
+                Bitmap bm = PhotoBitmapUtil.rotaingImageView(mCameraImagePath, BitmapFactory.decodeFile(mCameraImagePath)) ;
+
+                // 使用图片路径加载
+                ivPhoto.setImageBitmap(bm);
+
+
+                //通知相册更新
+                MediaStore.Images.Media.insertImage(MainActivity.this.getContentResolver(), bm, mCameraImagePath, null);
+                Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                Uri uri = Uri.fromFile(new File(mCameraImagePath));
+                intent.setData(uri);
+                MainActivity.this.sendBroadcast(intent);
+
+
+            } else {
+                Toast.makeText(this,"取消",Toast.LENGTH_LONG).show();
+            }
+
+
+
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/aliyun_identity_demo/util/PhotoBitmapUtil.java
@@ -0,0 +1,126 @@
+package com.example.aliyun_identity_demo.util;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class PhotoBitmapUtil {
+    /**
+     * 把原图按1/10的比例压缩
+     *
+     * @param path 原图的路径
+     * @return 压缩后的图片
+     */
+    public static Bitmap getCompressPhoto(String path) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+ /*       options.inJustDecodeBounds = false;
+        options.inSampleSize = 10;  // 图片的大小设置为原来的十分之一*/
+        Bitmap bmp = BitmapFactory.decodeFile(path, options);
+        options = null;
+        return bmp;
+    }
+
+    /**
+     * 读取照片旋转角度
+     *
+     * @param path 照片路径
+     * @return 角度
+     */
+    public static int readPictureDegree(String path) {
+        int degree = 0;
+        try {
+            ExifInterface exifInterface = new ExifInterface(path);
+            int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    degree = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    degree = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    degree = 270;
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return degree;
+    }
+
+    /**
+     * 旋转图片
+     * @param bitmap 图片对象
+     * @return 旋转后的图片
+     */
+    public static Bitmap rotaingImageView( String path,Bitmap bitmap) {
+        int angle = readPictureDegree(path);
+        Bitmap returnBm = null;
+        // 根据旋转角度，生成旋转矩阵
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        try {
+            // 将原始图片按照旋转矩阵进行旋转，并得到新的图片
+            returnBm = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+        } catch (OutOfMemoryError e) {
+        }
+        if (returnBm == null) {
+            returnBm = bitmap;
+        }
+
+        saveBitmap(path,bitmap);
+
+
+        if (bitmap != returnBm) {
+            bitmap.recycle();
+        }
+        return returnBm;
+    }
+
+    public static File saveBitmap(String path, Bitmap bm) {
+        File f = new File(path);
+        if (f.exists()) {
+            f.delete();
+        }
+        try {
+            FileOutputStream out = new FileOutputStream(f);
+            bm.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.flush();
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return f;
+    }
+
+
+
+    /**
+     * 图片镜像翻转
+     * @param bmp
+     * @return
+     */
+    public static Bitmap convertBmp(Bitmap bmp) {
+        int w = bmp.getWidth();
+        int h = bmp.getHeight();
+
+        Matrix matrix = new Matrix();
+        matrix.postScale(-1, 1); // 镜像水平翻转
+        Bitmap convertBmp = Bitmap.createBitmap(bmp, 0, 0, w, h, matrix, true);
+
+        return convertBmp;
+    }
+
+
+
+}
